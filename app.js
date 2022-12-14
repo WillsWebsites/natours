56eb3fda9d016d661dfd24978ac095fa78cdd407
William Schaefermeyer <william.schaefermeyer@gmail.com>
2022-12-14 11:35:06 -0700

    Finishing security and authentication

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+
 const AppError = require('./utils/appError')
 const GlobalErrorHandler = require('./controller/errorController')
 const tourRouter = require('./routes/tourRoutes')
@@ -27,12 +31,28 @@ app.use('/api', limiter)
 app.use(express.json({ limit: '10kb' }))
 
 // Data Sanitization against NoSQL query injection
+app.use(mongoSanitize())
 
 // Data Sanitization against XSS
+app.use(xss())
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuatintity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+)
 
 app.use(express.static(`${__dirname}/public`))
 
-app.use((req, res, next) => {
+app.use((req, _, next) => {
   req.requestTime = new Date().toISOString()
   next()
 })
@@ -40,7 +60,7 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
-app.all('*', (req, res, next) => {
+app.all('*', (req, _, next) => {
   next(new AppError(`Couldn't find the requested route: ${req.originalUrl}`, 404))
 })
 
